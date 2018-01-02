4a05c8d287bb7bbf5c57103a4cfa4e719f9d0e30
Oleg Chirukhin <olegchiruhin@gmail.com>
2018-01-02 17:22:38 +0700

    initial commit

---
+++ smartapi-web/src/main/java/com/textorio/habrahabr/smartapiweb/SmartapiWebApplication.java
@@ -0,0 +1,12 @@
+package com.textorio.habrahabr.smartapiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartapiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartapiWebApplication.class, args);
+	}
+}
---
+++ smartapi-web/src/test/java/com/textorio/habrahabr/smartapiweb/SmartapiWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.textorio.habrahabr.smartapiweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartapiWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
