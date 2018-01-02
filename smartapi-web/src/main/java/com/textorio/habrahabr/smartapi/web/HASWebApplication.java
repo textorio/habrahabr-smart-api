4b37e025cdb9bc020afdd81b022dbb16ca3ddff9
Oleg Chirukhin <olegchiruhin@gmail.com>
2018-01-02 17:29:15 +0700

    refactor package name

--- smartapi-web/src/main/java/com/textorio/habrahabr/smartapi/web/HASWebApplication.java
+++ smartapi-web/src/main/java/com/textorio/habrahabr/smartapi/web/HASWebApplication.java
@@ -1,12 +1,12 @@
-package com.textorio.habrahabr.smartapiweb;
+package com.textorio.habrahabr.smartapi.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartapiWebApplication {
+public class HASWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartapiWebApplication.class, args);
+		SpringApplication.run(HASWebApplication.class, args);
 	}
 }
