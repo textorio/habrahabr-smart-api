8fdc9e0ef3c8e8095e3d41001b90dd090bd8c063
Oleg Chirukhin <olegchiruhin@gmail.com>
2018-01-02 18:28:50 +0700

    prepare to implement headless chrome

---
+++ smartapi-core/src/main/java/com/textorio/habrahabr/smartapi/core/webdriver/Webdriver.java
@@ -0,0 +1,9 @@
+package com.textorio.habrahabr.smartapi.core.webdriver;
+
+/**
+ * https://sites.google.com/a/chromium.org/chromedriver
+ * https://sites.google.com/a/chromium.org/chromedriver/downloads
+ */
+public class Webdriver {
+
+}
