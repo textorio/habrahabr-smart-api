fa362e64737a655ca224ff8d3b7c6234c9594b6f
Oleg Chirukhin <hedin.pr@gmail.com>
2018-01-06 17:55:57 +0700

    moved system directories to a separate DirectoryLocator

---
+++ smartapi-core/src/main/java/com/textorio/habrahabr/smartapi/core/lang/DirLocator.java
@@ -0,0 +1,20 @@
+package com.textorio.habrahabr.smartapi.core.lang;
+
+import java.io.File;
+
+public class DirLocator {
+    public static final String TEXTORIO_SETTINGS_DIR = ".textorio";
+    public static final String TEXTORIO_HABR_SETTINGS_DIR = "habrahabr";
+
+    /**
+     * https://stackoverflow.com/questions/585534/what-is-the-best-way-to-find-the-users-home-directory-in-java
+     * @return
+     */
+    public static String getUserHome() {
+        return System.getProperty("user.home");
+    }
+
+    public static String getSettingsFilePath(String username) {
+        return getUserHome() + File.separator + TEXTORIO_SETTINGS_DIR + File.separator + TEXTORIO_HABR_SETTINGS_DIR + File.separator + username;
+    }
+}
--- smartapi-core/src/main/java/com/textorio/habrahabr/smartapi/core/lang/Sys.java
+++ smartapi-core/src/main/java/com/textorio/habrahabr/smartapi/core/lang/Sys.java
@@ -11,19 +11,10 @@ import java.util.Properties;
 
 public class Sys {
     private static Logger logger = LoggerFactory.getLogger(Sys.class);
-    public static final String TEXTORIO_SETTINGS_DIR = ".textorio";
-    public static final String TEXTORIO_HABR_SETTINGS_DIR = "habrahabr";
-    /**
-     * https://stackoverflow.com/questions/585534/what-is-the-best-way-to-find-the-users-home-directory-in-java
-     * @return
-     */
-    public static String getUserHome() {
-        return System.getProperty("user.home");
-    }
 
     public static synchronized Properties getUserSettings(String username) {
         Properties result = new Properties();
-        String settingsFilePath = getSettingsFilePath(username);
+        String settingsFilePath = DirLocator.getSettingsFilePath(username);
         try (FileInputStream input = new FileInputStream(settingsFilePath)) {
             result.load(input);
         } catch (Exception e) {
@@ -34,7 +25,7 @@ public class Sys {
 
     public static synchronized void saveUserSettings(Properties props) {
         String username = props.getProperty("username");
-        String settingsFilePath = getSettingsFilePath(username);
+        String settingsFilePath = DirLocator.getSettingsFilePath(username);
         File settingsFile = new File(settingsFilePath);
         try (OutputStream out = new FileOutputStream( settingsFile )) {
             props.store(out, "Textorio settings for Habrahabr");
@@ -65,9 +56,5 @@ public class Sys {
         userSettings.setProperty(fieldName,Integer.toString(newValue));
         Sys.saveUserSettings(userSettings);
         return oldValue;
-    }
-
-    public static String getSettingsFilePath(String username) {
-        return getUserHome() + File.separator + TEXTORIO_SETTINGS_DIR + File.separator + TEXTORIO_HABR_SETTINGS_DIR + File.separator + username;
     }
 }
